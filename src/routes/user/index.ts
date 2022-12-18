de20cde6b3a28e19a137070fec2b6da99d78cae2
mangochieu <hieumn2001@gmail.com>
2022-12-18 20:12:32 +0700

    test

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -1,18 +1,17 @@
 import { Router } from "express";
 
-import controller from "../../controllers/user";
 import { validateToken } from "../../middleware/validate";
 import cartController from "../../controllers/cart";
 const router = Router();
 
-router.post("/signup", controller.signup);
-router.post("/login", controller.login);
-router.get("/getself", validateToken, controller.getSelfUser);
-router.post("/refresh_token", controller.refreshToken);
-router.patch("/update_self", validateToken, controller.updateSelfUser);
-router.post("/logout", validateToken, controller.logout);
+// router.post("/signup", controller.signup);
+// router.post("/login", controller.login);
+// router.get("/getself", validateToken, controller.getSelfUser);
+// router.post("/refresh_token", controller.refreshToken);
+// router.patch("/update_self", validateToken, controller.updateSelfUser);
+// router.post("/logout", validateToken, controller.logout);
 
-//Cart routes
+//Cart routes2
 router.post("/cart/add", validateToken, cartController.addToCart);
 router.post("/cart/remove", validateToken, cartController.removeFromCart);
 router.post(
